618f943313d781c59e97dd5db8f45a08b31bdf4d
Dahlia Kadri <dahliakadri@gmail.com>
2019-10-20 22:39:40 +0000

    get player name

---
+++ guessing-game/game.py
@@ -0,0 +1,13 @@
+"""A number-guessing game."""
+
+#get player name
+player_name = input("Welcome to the Guessing Game. What is your name?")
+
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
