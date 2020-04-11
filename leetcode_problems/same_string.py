30ce068fb2d99f66df44d360b89e5bad6145126c
dahliakadri <dahliakadri@gmail.com>
2020-04-11 19:03:33 +0000

    second try same_string

--- leetcode_problems/same_string.py
+++ leetcode_problems/same_string.py
@@ -1,3 +1,21 @@
 def same_strings(s, t):
 	if s == t:
 		return True
+	s = list(s)
+	t = list(t)
+	for index, letter in enumerate(s):
+		if letter == "#":
+			s = s.pop(index-1)
+
+	for index, letter in enumerate(t):
+		if letter == "#":
+			t = t.pop(index-1)
+	if s == t:
+		return True
+	else:
+		return False
+
+s = "ab#c"
+t = "ad#c"
+
+print(same_strings(s,t))
