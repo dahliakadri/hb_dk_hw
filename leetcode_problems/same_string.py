5b8eb26cc1fe2f9e1997145bc1fd481ef1ed5bb6
dahliakadri <dahliakadri@gmail.com>
2020-04-11 20:01:09 +0000

    add stack solution

--- leetcode_problems/same_string.py
+++ leetcode_problems/same_string.py
@@ -34,3 +34,45 @@ print(same_strings(a,b))
 print(same_strings(c,d))
 
 print(same_strings(e,f))
+
+
+def same_strings_stack(s, t):
+	s_list = []
+	t_list = []
+	for letter in s:
+		if letter != "#":
+			s_list.append(letter)
+		elif len(s_list) == 0:
+			continue
+		else:
+			s_list.pop()
+
+	for letter in t:
+		if letter != "#":
+			t_list.append(letter)
+		elif len(t_list) == 0:
+			continue
+		else:
+			t_list.pop()
+
+	if s_list == t_list:
+		return True
+	else:
+		return False
+
+s = "ab#c"
+t = "ad#c"
+a = 'ab##'
+b = 'c#d#'
+c = 'a##c'
+d = '#a#c'
+e = 'a#c'
+f = 'b'
+
+print(same_strings_stack(s,t))
+
+print(same_strings_stack(a,b))
+
+print(same_strings_stack(c,d))
+
+print(same_strings_stack(e,f))
