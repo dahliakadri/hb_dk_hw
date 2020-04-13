5dedfadda2fdf84813bf6632dffb7a93aa861a65
dahliakadri <dahliakadri@gmail.com>
2020-04-13 21:40:31 +0000

    add practice session

---
+++ leetcode_problems/PracticeInterview_JM.py
@@ -0,0 +1,26 @@
+a = 25
+b = 10
+c = 5
+d = 1
+
+def coin_machine(cents):
+#Given a certain amount of money of $.87 return quarters dimes nickles and pennies that amount to that.
+#return optimal solution with least number of coins
+
+#cents / % a = 0 then w = cents/ a
+
+	if cents % a == 0:
+		w = (cents / a)
+		x , y , z = 0, 0 , 0
+	else:
+		w = (cents // a)
+		cents = cents - (w*a)
+		if cents % b  == 0:
+			x = (cents / b)
+			y, z = 0, 0
+#else  take remainder of w and check % b
+
+
+	return w, "quarters", x, "dimes", y, "nickles", z , "pennies"
+
+print(coin_machine(85))
